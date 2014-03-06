d336691ccf5e544a374974bb02c8965e01626538
Simply Innovative <careers@siminov.com>
2014-03-06 08:30:35 -0800

    Siminov Connect integration

---
+++ src/siminov/hybrid/service/GenericService.java
@@ -0,0 +1,41 @@
+package siminov.hybrid.service;
+
+import siminov.connect.connection.ConnectionRequest;
+import siminov.connect.connection.ConnectionResponse;
+import siminov.connect.exception.ServiceException;
+import siminov.connect.service.Service;
+
+public class GenericService extends Service {
+
+	public void onServiceStart() {
+		
+	}
+
+	public void onServiceQueue() {
+		
+	}
+
+	public void onServicePause() {
+		
+	}
+
+	public void onServiceResume() {
+		
+	}
+
+	public void onServiceFinish() {
+		
+	}
+
+	public void onServiceApiInvoke(ConnectionRequest connectionRequest) {
+		
+	}
+
+	public void onServiceApiFinish(ConnectionResponse connectionResponse) {
+		
+	}
+
+	public void onServiceTerminate(ServiceException serviceException) {
+		
+	}
+}
